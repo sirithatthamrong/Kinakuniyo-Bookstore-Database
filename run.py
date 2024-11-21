372537349613e253529a678fc09fddc95bc1dec9
lingg <kanladaporn.sir@gmail.com>
2024-11-21 20:41:28 +0700

    Add render

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host=os.getenv('HOST', '0.0.0.0'), port=int(os.getenv('PORT', 5000)))
